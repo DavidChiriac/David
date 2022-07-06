85ed462d5c70febc60015fcc8e04476e48fdf353
DavidChiriac <chiriacdavid74@gmail.com>
2022-07-06 17:40:44 +0300

    routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,23 @@ import { PagesService } from './pages.service';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
+import { KnowledgeComponent } from './Knowledge/knowledge.component';
+import { HomeComponent } from './Home/home.component';
+import { ContactComponent } from './Contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     FooterComponent,
+    KnowledgeComponent,
+    HomeComponent,
+    ContactComponent,
   ],
   imports: [
     BrowserModule,
