cebf3082ac3a0be55eb8977408adbb927d974488
DavidChiriac <chiriacdavid74@gmail.com>
2022-07-07 10:59:04 +0300

    Home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
+import { HomeComponent } from './Home/home.component';
 import { ContactComponent } from './Contact/contact.component';
 import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { KnowledgeComponent } from './Knowledge/knowledge.component';
-import { HomeComponent } from './Home/home.component';
+import { AppComponent } from './app.component';
 
 const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'Home', component: HomeComponent},
   {path: 'Knowledge', component: KnowledgeComponent},
-  {path: 'Home', component:HomeComponent},
   {path: 'Contact', component:ContactComponent}
 ];
 
